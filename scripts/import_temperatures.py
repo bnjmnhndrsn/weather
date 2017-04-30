4201ec8a6227c0727424088287b365e7cdd91709
Ben Henderson <ben@nestio.com>
2017-04-29 23:08:41 -0600

    get data importing working

--- scripts/import_temperatures.py
+++ scripts/import_temperatures.py
@@ -1,5 +1,14 @@
+import django
+import sys, os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hava.settings")
+sys.path.append(os.path.join(os.path.realpath(os.path.dirname(__file__)), '..'))
+django.setup()
+
 import re
+from django.contrib.gis.geos import GEOSGeometry
 from decimal import Decimal
+from weather.models import WeatherLocation
 
 data = {}
 
@@ -47,8 +56,7 @@ def import_temperatures(filename, key):
                 stripped = month.strip()
                 temperature = Decimal(stripped[:-1]) / Decimal(10)
                 months_formatted.append({
-                    'temperature': temperature,
-                    'grade': stripped[-1]
+                    'temperature': str(temperature)
                 })
             station_data = data.get(station, {})
             station_data[key] = months_formatted
@@ -56,9 +64,25 @@ def import_temperatures(filename, key):
 
 import_stations('./data/allstations.txt')
 import_zipcodes('./data/zipcodes-normals-stations.txt')
-import_temperatures('./data/mly-tmax-normal.txt', 'max')
-import_temperatures('./data/mly-tmin-normal.txt', 'min')
+import_temperatures('./data/mly-tmax-normal.txt', 'max_data')
+import_temperatures('./data/mly-tmin-normal.txt', 'min_data')
+
+WeatherLocation.objects.all().delete()
 
 for key, val in data.iteritems():
-    print key
-    print val
+    if not val.get('min_data') and not val.get('max_data'):
+        continue
+
+    data_dict = {
+        'source': WeatherLocation.SOURCE_NOAA,
+        'source_idenitifier': val['id'],
+        'location': GEOSGeometry('POINT(%s %s)' % (val['longitude'], val['latitude']), srid=4269),
+        'place_name': val.get('place_name', ''),
+        'station_name': val.get('station_name', ''),
+        'subnational_entity': val.get('state', ''),
+        'country': 'US',
+        'postal_code':  val.get('zipcode', ''),
+        'min_data': val['min_data'],
+        'max_data': val['max_data']
+    }
+    WeatherLocation.objects.create(**data_dict)
--- weather/models.py
+++ weather/models.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.gis.db.models import PointField
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class WeatherLocation(models.Model):
+    SOURCE_NOAA = 10
+
+    SOURCES = (
+        (SOURCE_NOAA, 'NOAA'),
+    )
+
+    source = models.IntegerField(choices=SOURCES)
+    source_idenitifier = models.CharField(max_length=30)
+    location = PointField(srid=4269)
+    place_name = models.CharField(null=False, blank=True, max_length=30)
+    station_name = models.CharField(null=False, blank=True, max_length=30)
+    subnational_entity = models.CharField(null=False, blank=True, max_length=30)
+    country = models.CharField(null=False, blank=False, max_length=30)
+    postal_code = models.CharField(null=False, blank=True, max_length=30)
+    min_data = JSONField(default=list)
+    max_data = JSONField(default=list)
