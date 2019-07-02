2ecbe2612e02739e2e2377c351e83e84575f4ab4
Ben Henderson <benjamin.c.henderson@gmail.com>
2019-07-02 16:42:55 -0600

    add in search by location

--- weather/urls.py
+++ weather/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^json/search/$', views.SearchView.as_view()),
-    url(r'^json/search/$', views.SearchView.as_view()),
-    url(r'^json/locations/(?P<weather_location_id>\d+)/$', views.WeatherLocationView.as_view())
-
+    url(r'^json/search-by-location/$', views.SearchByLocationView.as_view()),
+    url(r'^json/locations/(?P<weather_location_id>\d+)/$', views.WeatherLocationView.as_view()),
 ]
--- weather/views.py
+++ weather/views.py
@@ -6,9 +6,14 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views import View
 from django.views import View
+from django.contrib.gis.db.models import functions
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis import measure
 
 from weather.models import WeatherLocation
 from weather.serializers import serialize_search, serialize_detail
+
+MAX_MILE_DISTANCE = 15
 
 def index(request):
     return render(request, 'weather/index.html', {})
@@ -20,12 +25,50 @@ class SearchView(View):
         query = Q(postal_code__istartswith=stripped) | Q(place_name__istartswith=stripped)
         return WeatherLocation.objects.filter(query).order_by('place_name', 'subnational_entity')[0:20]
 
+    def _filter_by_lat_lng(self, lat, lng):
+        try:
+            location = GEOSGeometry('POINT(%s %s)' % (lng, lat), srid=4269)
+        except ValueError:
+            return
+
+        return WeatherLocation.objects.annotate(
+            distance=Distance('location', location)
+        ).order_by('distance')[0:20]
+
     def get(self, request):
         results = []
         if 'query' in request.GET:
             results = self._filter_by_query(request.GET['query'])
 
+        if 'lat' in request.GET and 'lng' in request.GET:
+            results = self._filter_by_lat_lng(request.GET['lat'], request.GET['lng'])
+
         serialized = serialize_search(results)
+        return JsonResponse(serialized, safe=False)
+
+class SearchByLocationView(View):
+    def get(self, request):
+        results = []
+        if not 'lat' in request.GET and not 'lng' in request.GET:
+            return JsonResponse({'error': 'missing lat/lng'}, status=400)
+
+        try:
+            location = GEOSGeometry('POINT(%s %s)' % (request.GET['lng'], request.GET['lat']), srid=4269)
+        except ValueError:
+            return JsonResponse({'error': 'invalid lat/lng'}, status=400)
+
+
+        result = WeatherLocation.objects.annotate(
+            distance=functions.Distance('location', location)
+        ).order_by('distance').first()
+
+        if not result:
+            return JsonResponse({'error': 'no matches'}, status=400)
+
+        if result.distance.mi > MAX_MILE_DISTANCE:
+            return JsonResponse({'error': 'no matches within {} miles'.format(MAX_MILE_DISTANCE)}, status=400)
+
+        serialized = serialize_detail(result)
         return JsonResponse(serialized, safe=False)
 
 class WeatherLocationView(View):
